e6b71a82a2e7605c449f0f454acf1fe7f00ec999
hadrien <hadrien.reboul@pm.me>
2018-10-04 10:40:52 +0200

    Added cards, searchbar and + button in home pagae Added ios simulator

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,14 +1,27 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import { LimitTo } from '../../app/limitTo.pipe';
 
 @Component({
   selector: 'page-home',
-  templateUrl: 'home.html'
+  templateUrl: 'home.html'   
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  memories : any[] = [
+    {
+        img : "nin-live.png",
+        title : "This is a cool title about a very long story that may take several lines to write",
+        content : "Here are some nice content about the nice title! Like, share, enjoy! Here are some nice content about the nice title! Like, share, enjoy! Here are some nice content about the nice title! Like, share, enjoy!Here are some nice content about the nice title! Like, share, enjoy!Here are some nice content about the nice title! Like, share, enjoy!Here are some nice content about the nice title! Like, share, enjoy!"
+    },
+    {
+        img : "nin-live.png",
+        title : "This is a cool title about a very long story that may take several lines to write",
+        content : "Here are some nice content about the nice title! Like, share, enjoy! Here are some nice content about the nice title! Like, share, enjoy! Here are some nice content about the nice title! Like, share, enjoy!Here are some nice content about the nice title! Like, share, enjoy!Here are some nice content about the nice title! Like, share, enjoy!Here are some nice content about the nice title! Like, share, enjoy!"
+    } 
+  ]; 
 
+  constructor(public navCtrl: NavController) { 
   }
 
-}
+} 
