c95666b53dfc976bbc39d5921c6a1d1df5e62dfd
Furl0w <rainbowphoque@gmail.com>
2018-10-10 11:06:01 +0200

    Delete memory, adding date field to memories

--- src/app/sql.service.ts
+++ src/app/sql.service.ts
@@ -8,6 +8,7 @@ export type Memory = {
     Location: string;
     Mark: number;
     Tags: string;
+    Date : Date;
 }
 
 //TODO error handling
@@ -32,28 +33,28 @@ export class DataBaseService {
         } catch (error) {
             console.log(error);
         }
-        
+
         //return this.db.executeSql('drop table memories', []);
-        
-        let promise = this.db.executeSql('create table if not exists memories(Title VARCHAR(32), Description VARCHAR(550),Location VARCHAR(150),Mark INT, Tags VARCHAR(150))', []);
+
+        let promise = this.db.executeSql('create table if not exists memories(Title VARCHAR(32), Description VARCHAR(550),Location VARCHAR(150),Mark INT, Tags VARCHAR(150), Date VARCHAR(100))', []);
         promise.then(() => console.log('SQLite ready'));
         promise.catch((e) => console.log(e));
         return promise;
-        
+
     }
 
     async insertNewMemory(memory: any) {
         await this.dbReady;
 
-        let query = "INSERT INTO memories (Title, Description) VALUES (?,?)";
-        return this.db.executeSql(query, [memory.title, memory.description]);
+        let query = "INSERT INTO memories (Title, Description,Date) VALUES (?,?,?)";
+        return this.db.executeSql(query, [memory.title, memory.description, memory.Date.toString()]);
     }
 
-    async selectMemories(id? : string) {
+    async selectMemories(id?: number) {
         await this.dbReady;
         let params = [];
         let query = '';
-        if(id){
+        if (id) {
             params = [id];
             query = 'select rowid,* from memories where ROWID = ?';
         } else {
@@ -69,54 +70,61 @@ export class DataBaseService {
         return this.db.executeSql(query, [memory.title, memory.description, memory.id]);
     }
 
+    async deleteMemory(memory: any) {
+        await this.dbReady;
+
+        let query = "DELETE FROM memories WHERE ROWID = ?";
+        return this.db.executeSql(query, [memory.id]);
+    }
+
 }
 
 
 
 @Injectable()
-export class memoryUpdater {        
+export class memoryUpdater {
 
     memories: Memory[] = [];
 
     constructor(protected DBS: DataBaseService) {
         this.DBS.selectMemories().then((result) => {
-            for(let i = 0; i < result.rows.length; i++) {
+            for (let i = 0; i < result.rows.length; i++) {
                 let row = result.rows.item(i);
-                if(row !== undefined)
+                if (row !== undefined)
+                    row.Date = new Date(row.Date);
                     this.memories.push(row);
-            }                
+            }
         }).catch(e => console.log(e));
     }
 
-    createNewMemory(title:string, description:string) {
-        let memory = {
-            title: title,
-            description: description
-        };
-
+    async createNewMemory(memory: Memory) {
         return this.DBS.insertNewMemory(memory).then(
-            (result) => {   
-                this.DBS.selectMemories(result.insertId).then((result)=>{
+            (result) => {
+                this.DBS.selectMemories(result.insertId).then((result) => {
                     this.memories.push(result.rows.item[0]);
-                })           
-                .catch(e => console.log(e));
+                })
+                    .catch(e => console.log(e));
             },
             (e) => console.error(e)
         );
     }
 
+    filterMemories() {
+
+    }
+
+    async deleteMemory(memory) {
+        return this.DBS.deleteMemory(memory).then((result) => {
+            this.memories.splice(this.memories.indexOf(memory), 1);
+        })
+    }
+
     async updateMemory(memory) {
         return this.DBS.updateMemory(memory)
             .then((result) => {
-                for (let i = 0; i < this.memories.length; i++) {
-                    if (this.memories[i].id == result.rows.item[0].id) {
-                        this.memories.splice(i, 1);
-                        this.memories.push(result.rows.item[0]);
-                        break;
-                    }
-                }
+                this.memories[this.memories.indexOf(memory)] = result.rows.item[0]
             })
             .catch((e) => console.error(e));
     }
-    
+
 }
