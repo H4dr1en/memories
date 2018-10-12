8d63344c13137f2d885182561e6d93eeec37f62c
Furl0w <rainbowphoque@gmail.com>
2018-10-12 16:42:08 +0200

    correcting update tags

--- src/pages/edit-memory/edit-memory.ts
+++ src/pages/edit-memory/edit-memory.ts
@@ -18,14 +18,31 @@ import { memoryProvider, Memory } from '../../app/sql.service'
 export class EditMemoryPage {
 
     mem: Memory
+    tags: any[] = []
+    tagsToRemove: any[] = []
+    tagsToAdd: any[] = []
 
     constructor(public navCtrl: NavController, public navParams: NavParams, public memoryProvider: memoryProvider) {
         this.mem = this.navParams.get('mem');
+        this.tags = this.mem.Tags
     }
 
     editMemory() {
-        this.memoryProvider.updateMemory(this.mem);
+        this.memoryProvider.updateMemory(this.mem,this.tagsToAdd,this.tagsToRemove);
         this.navCtrl.pop();
+    }
+
+    onTagChange() {
+        this.tags.forEach(tag =>{
+            if(this.mem.Tags.indexOf(tag) === -1 && this.tagsToAdd.indexOf(tag) === -1){
+                this.tagsToAdd.push(tag)
+            }
+        })
+        this.mem.Tags.forEach(tag =>{
+            if(this.tags.indexOf(tag) === -1 && this.tagsToRemove.indexOf(tag) === -1){
+                this.tagsToRemove.push(tag)
+            }
+        })
     }
 
     ionViewDidLoad() {
