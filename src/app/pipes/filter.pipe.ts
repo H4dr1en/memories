2e57ab0b598c27756738f8b9aaa7ab905f7ce892
H4dr1en <h4dr1en@pm.me>
2018-10-25 14:22:41 +0200

    try catch on filters

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -9,30 +9,36 @@ import { Memory } from '../memory.provider';
 export class FilterPipe implements PipeTransform {
     transform(memories: Memory[], filters: any): Memory[] {
 
-        if (memories === undefined || memories === null) {
-            return [];
-        }
+        try {
 
-        if (filters.tags.length > 0) {
-                memories = memories.filter(function (el) {   
-                    /*  
-                    if(filters.tags.some(tag1 => el.Tags.some(tag2 => tag1 == tag2))) {
-                        return el.Title.toLowerCase().includes(filters.searchTerm.toLowerCase());
-                    } else {
-                        return false;
-                    }
-                    */
-                   return filters.tags.some(tag1 => el.Tags.some(tag2 => tag1 == tag2));
-                });
+            if (memories === undefined || memories === null) {
+                return [];
             }
-        
-        memories = memories.filter(function(el) {
-            return el.Title.toLowerCase().includes(filters.searchTerm.toLowerCase());
-        });
+    
+            if (filters.tags.length > 0) {
+                    memories = memories.filter(function (el) {   
+                        /*  
+                        if(filters.tags.some(tag1 => el.Tags.some(tag2 => tag1 == tag2))) {
+                            return el.Title.toLowerCase().includes(filters.searchTerm.toLowerCase());
+                        } else {
+                            return false;
+                        }
+                        */
+                       return filters.tags.some(tag1 => el.Tags.some(tag2 => tag1 == tag2));
+                    });
+                }
+            
+            memories = memories.filter(function(el) {
+                return el.Title.toLowerCase().includes(filters.searchTerm.toLowerCase());
+            });
+    
+            memories = memories.filter(function(el) {
+                return filters.marks.lower <= el.Mark && el.Mark <= filters.marks.upper;
+            })
 
-        memories = memories.filter(function(el) {
-            return filters.marks.lower <= el.Mark && el.Mark <= filters.marks.upper;
-        })
+        } catch (e) {
+            console.error(e);
+        }        
 
         return memories;
     }
