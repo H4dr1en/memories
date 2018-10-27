c35ceaadc0230a21a16d5298ec0dbf9ad2bb4327
H4dr1en <h4dr1en@pm.me>
2018-10-27 11:50:35 +0200

    removed comments from fitler pipe

--- src/app/pipes/filter.pipe.ts
+++ src/app/pipes/filter.pipe.ts
@@ -14,35 +14,28 @@ export class FilterPipe implements PipeTransform {
             if (memories === undefined || memories === null) {
                 return [];
             }
-    
+
             if (filters.tags.length > 0) {
-                    memories = memories.filter(function (el) {   
-                        /*  
-                        if(filters.tags.some(tag1 => el.Tags.some(tag2 => tag1 == tag2))) {
-                            return el.Title.toLowerCase().includes(filters.searchTerm.toLowerCase());
-                        } else {
-                            return false;
-                        }
-                        */
-                       return filters.tags.some(tag1 => el.Tags.some(tag2 => tag1 == tag2));
-                    });
-                }
-            
-            memories = memories.filter(function(el) {
+                memories = memories.filter(function (el) {
+                    return filters.tags.some(tag1 => el.Tags.some(tag2 => tag1 == tag2));
+                });
+            }
+
+            memories = memories.filter(function (el) {
                 return el.Title.toLowerCase().includes(filters.searchTerm.toLowerCase());
             });
-    
-            memories = memories.filter(function(el) {
+
+            memories = memories.filter(function (el) {
                 return filters.marks.lower <= el.Mark && el.Mark <= filters.marks.upper;
             })
 
-            memories = memories.filter(function(el){
+            memories = memories.filter(function (el) {
                 return (filters.onlyBookmark == true && el.Bookmark == 1) || filters.onlyBookmark == false
             })
 
         } catch (e) {
             console.error(e);
-        }        
+        }
 
         return memories;
     }
